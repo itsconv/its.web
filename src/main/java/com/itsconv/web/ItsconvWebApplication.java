a5a834e3f73a33863a6ce8f1a2ac35cb8aa11e14
jylim26 <yejong98@gmail.com>
2026-02-23 15:35:09 +0900

    first init

---
+++ src/main/java/com/itsconv/web/ItsconvWebApplication.java
@@ -0,0 +1,13 @@
+package com.itsconv.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsconvWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItsconvWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itsconv/web/ItsconvWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsconv.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsconvWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
