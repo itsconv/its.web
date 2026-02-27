9ca450516610d3930f2c1d295dbb549c900a8ca9
jylim <yejong98@gmail.com>
2026-02-27 19:00:34 +0900

    feat: 프론트 화면 라우팅 및 템플릿 스타일 반영

---
+++ src/main/java/com/itsconv/web/view/front/FrontViewController.java
@@ -0,0 +1,15 @@
+package com.itsconv.web.view.front;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/front")
+public class FrontViewController {
+
+    @GetMapping("/trading-center")
+    public String tradingCenter() {
+        return "front/trading_center";
+    }
+}
